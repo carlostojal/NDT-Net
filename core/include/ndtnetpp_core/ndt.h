7dede61ca6265c717bd67d4e7edfe317fa179e68
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-08 12:47:20 +0100

    find the point cloud limits in each direction of each axis

--- core/include/ndtnetpp_core/ndt.h
+++ core/include/ndtnetpp_core/ndt.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <float.h>
 
 /*! \brief Print a matrix to the standard output. 
     \param matrix Pointer to the matrix. Row-major order.
@@ -7,3 +8,46 @@
     \param cols Number of columns in the matrix.
 */
 void print_matrix(float *matrix, int rows, int cols);
+
+/*! \brief Test matrix modification by reference. Doubles the matrix values. 
+    \param matrix Pointer to the matrix. Row-major order.
+    \param rows Number of rows in the matrix.
+    \param cols Number of columns in the matrix.
+*/
+void test_modify_matrix(float *matrix, int rows, int cols);
+
+/*! \brief Get the point cloud limits in each dimension. The values will be assigned by reference.
+    \param point_cloud Pointer to the point cloud.
+    \param point_dim Point dimension. (Example: 3 for xyz points).
+    \param num_points Number of points in the point cloud.
+    \param max_x Maximum value in the "x" dimension. Will be overwritten.
+    \param max_y Maximum value in the "y" dimension. Will be overwritten.
+    \param max_z Maximum value in the "z" dimension. Will be overwritten.
+    \param min_x Minimum value in the "x" dimension. Will be overwritten.
+    \param min_y Minimum value in the "y" dimension. Will be overwritten.
+    \param min_z Minimum value in the "z" dimension. Will be overwritten.
+*/
+void get_pointcloud_limits(float *point_cloud, int point_dim, unsigned long num_points, 
+                        float *max_x, float *max_y, float *max_z,
+                        float *min_x, float *min_y, float *min_z);
+
+/*! \brief TODO: Estimate the normal distributions on the point cloud. Estimate a normal distribution per voxel of size "voxel_size".
+    \param point_cloud Pointer to the point cloud.
+    \param voxel_size Voxel size for distribution sampling.
+    \param max_x Maximum value in the "x" dimension.
+    \param max_y Maximum value in the "y" dimension.
+    \param max_z Maximum value in the "z" dimension.
+    \param min_x Minimum value in the "x" dimension.
+    \param min_y Minimum value in the "y" dimension.
+    \param min_z Minimum value in the "z" dimension.
+*/
+void estimate_ndt(float *point_cloud, float voxel_size, float max_x, float max_y, float max_z,
+                float min_x, float min_y, float min_z);
+
+/*! \brief TODO: Downsample the input point cloud with NDT.
+    \param point_cloud Pointer to the point cloud.
+    \param point_dim Point dimension. (Example: 3 for xyz points).
+    \param num_points Number of points in the input point cloud.
+    \param num_desired_points Number of desired points after sampling.
+ */
+void ndt_downsample(float *point_cloud, int point_dim, unsigned long num_points, unsigned long num_desired_points);
--- core/src/ndt.c
+++ core/src/ndt.c
@@ -9,3 +9,51 @@ void print_matrix(float *matrix, int rows, int cols) {
         printf("\n");
     }
 }
+
+void test_modify_matrix(float *matrix, int rows, int cols) {
+    
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            matrix[i*cols + j] = matrix[i*cols + j] * 2;
+        }
+    }
+}
+
+void get_pointcloud_limits(float *point_cloud, int point_dim, unsigned long num_points,
+                        float *max_x, float *max_y, float *max_z,
+                        float *min_x, float *min_y, float *min_z) {
+
+    *max_x = FLT_MIN;
+    *max_y = FLT_MIN;
+    *max_z = FLT_MIN;
+    *min_x = FLT_MIN;
+    *min_x = FLT_MIN;
+    *min_y = FLT_MIN;
+    *min_z = FLT_MIN;
+
+    // iterate over the points
+    for(unsigned long i = 0; i < num_points; i++) {
+
+        // verify the maximum x
+        if(point_cloud[i*point_dim] > *max_x)
+            *max_x = point_cloud[i*point_dim];
+        // verify the minimum x
+        if(point_cloud[i*point_dim] < *min_x)
+            *min_x = point_cloud[i*point_dim];
+
+        // verify the maximum y
+        if(point_cloud[i*point_dim + 1] > *max_y)
+            *max_y = point_cloud[i*point_dim + 1];
+        // verify the minimum y
+        if(point_cloud[i*point_dim + 1] < *min_y)
+            *min_y = point_cloud[i*point_dim + 1];
+
+        // verify the maximum z
+        if(point_cloud[i*point_dim + 2] > *max_z)
+            *max_z = point_cloud[i*point_dim + 2];
+        // verify the minimum z
+        if(point_cloud[i*point_dim + 2] < *min_z)
+            *min_z = point_cloud[i*point_dim + 2];
+        
+    }
+}
