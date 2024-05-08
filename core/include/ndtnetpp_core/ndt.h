4d649df7215f15d3a10ee629a93a4d113f03201a
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-08 19:55:19 +0100

    implement utilities to convert between metric and voxel space

--- core/include/ndtnetpp_core/ndt.h
+++ core/include/ndtnetpp_core/ndt.h
@@ -2,9 +2,18 @@
 #include <pthread.h>
 #include <float.h>
 #include <math.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
 
 #define NUM_PCL_WORKERS 8 // number of workers for bulk point cloud processing tasks
 #define PCL_DOWNSAMPLE_UPPER_THRESHOLD 0.2f // upper threshold percentage for the voxel grid downsampling
+
+struct normal_distribution_t {
+    float *mean; // xyz mean of the distribution (3-d)
+    float *covariance; // flattened covariance matrix (9-d)
+    long num_samples; // number of samples
+};
 
 /*! \brief Print a matrix to the standard output. 
     \param matrix Pointer to the matrix. Row-major order.
@@ -55,18 +64,45 @@ void estimate_voxel_size(unsigned long num_desired_voxels,
                         float *voxel_size,
                         int *len_x, int *len_y, int *len_z);
 
+
+/*! \brief Convert a point from metric space to voxel space (indexes).
+    \param point Pointer to the point.
+    \param voxel_size Voxel size.
+    \param len_x Number of voxels in the "x" dimension.
+    \param len_y Number of voxels in the "y" dimension.
+    \param len_z Number of voxels in the "z" dimension.
+    \param voxel_x Voxel index in the "x" dimension. Will be overwritten.
+    \param voxel_y Voxel index in the "y" dimension. Will be overwritten.
+    \param voxel_z Voxel index in the "z" dimension. Will be overwritten.
+*/
+int metric_to_voxel_space(float *point, float voxel_size,
+                            int len_x, int len_y, int len_z,
+                            unsigned int *voxel_x, unsigned int *voxel_y, unsigned int *voxel_z);
+
+
+/*! \brief Convert a point from voxel space (indexes) to metric space.
+    \param voxel_x Voxel index in the "x" dimension.
+    \param voxel_y Voxel index in the "y" dimension.
+    \param voxel_z Voxel index in the "z" dimension.
+    \param len_x Number of voxels in the "x" dimension.
+    \param len_y Number of voxels in the "y" dimension.
+    \param len_z Number of voxels in the "z" dimension.
+    \param voxel_size Voxel size.
+    \param point Pointer to the point. Will be overwritten.
+*/
+void voxel_to_metric_space(unsigned int voxel_x, unsigned int voxel_y, unsigned int voxel_z,
+                            int len_x, int len_y, int len_z,
+                            float voxel_size, float *point);
+
 /*! \brief TODO: Estimate the normal distributions on the point cloud. Estimate a normal distribution per voxel of size "voxel_size".
     \param point_cloud Pointer to the point cloud.
     \param voxel_size Voxel size for distribution sampling.
-    \param max_x Maximum value in the "x" dimension.
-    \param max_y Maximum value in the "y" dimension.
-    \param max_z Maximum value in the "z" dimension.
-    \param min_x Minimum value in the "x" dimension.
-    \param min_y Minimum value in the "y" dimension.
-    \param min_z Minimum value in the "z" dimension.
+    \param len_x Number of voxels in the "x" dimension.
+    \param len_y Number of voxels in the "y" dimension.
+    \param len_z Number of voxels in the "z" dimension.
 */
-void estimate_ndt(float *point_cloud, float voxel_size, float max_x, float max_y, float max_z,
-                float min_x, float min_y, float min_z);
+int estimate_ndt(float *point_cloud, float voxel_size,
+                    int len_x, int len_y, int len_z);
 
 /*! \brief TODO: Downsample the input point cloud with NDT.
     \param point_cloud Pointer to the point cloud.
--- core/src/ndt.c
+++ core/src/ndt.c
@@ -77,3 +77,49 @@ void estimate_voxel_size(unsigned long num_desired_voxels,
 
     *voxel_size = floor(*voxel_size);
 }
+
+int metric_to_voxel_space(float *point, float voxel_size,
+                            int len_x, int len_y, int len_z,
+                            unsigned int *voxel_x, unsigned int *voxel_y, unsigned int *voxel_z) {
+
+    // the center voxel of the grid is at metric (0, 0, 0)
+
+    // find the origin of the grid in metric space
+    float x_origin, y_origin, z_origin;
+    x_origin = -((float) len_x / 2) * voxel_size;
+    y_origin = -((float) len_y / 2) * voxel_size;
+    z_origin = -((float) len_z / 2) * voxel_size;
+
+    // calculate the voxel indexes
+    *voxel_x = (unsigned int) floor((point[0] - x_origin) / voxel_size);
+    *voxel_y = (unsigned int) floor((point[1] - y_origin) / voxel_size);
+    *voxel_z = (unsigned int) floor((point[2] - z_origin) / voxel_size);
+
+    // check if the point is outside the grid
+    if(*voxel_x < 0 || *voxel_x >= len_x ||
+        *voxel_y < 0 || *voxel_y >= len_y ||
+        *voxel_z < 0 || *voxel_z >= len_z) {
+        fprintf(stderr, "Point outside the grid!\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+void voxel_to_metric_space(unsigned int voxel_x, unsigned int voxel_y, unsigned int voxel_z,
+                            int len_x, int len_y, int len_z,
+                            float voxel_size, float *point) {
+
+    // the center voxel of the grid is at metric (0, 0, 0)
+
+    // find the origin of the grid in metric space
+    float x_origin, y_origin, z_origin;
+    x_origin = -((float) len_x / 2) * voxel_size;
+    y_origin = -((float) len_y / 2) * voxel_size;
+    z_origin = -((float) len_z / 2) * voxel_size;
+
+    // calculate the point in metric space
+    point[0] = x_origin + voxel_x * voxel_size;
+    point[1] = y_origin + voxel_y * voxel_size;
+    point[2] = z_origin + voxel_z * voxel_size;
+}
