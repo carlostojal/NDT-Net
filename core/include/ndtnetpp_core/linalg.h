89f42275ba498692db5592e8ac37f59e7037377e
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-09 18:41:36 +0100

    define the header file for the linear algebra utilities

---
+++ core/include/ndtnetpp_core/linalg.h
@@ -0,0 +1,42 @@
+#ifndef LINALG_H_
+#define LINALG_H_
+
+#include <stdio.h>
+
+/*! \brief Multiply two matrices.
+    \param mat1 Pointer to the first matrix. Row-major order.
+    \param mat2 Pointer to the second matrix. Row-major order.
+    \param result Pointer to the result matrix. Row-major order.
+    \param rows1 Number of rows in the first matrix.
+    \param cols1 Number of columns in the first matrix.
+    \param rows2 Number of rows in the second matrix.
+    \param cols2 Number of columns in the second matrix.
+
+*/
+void matmul(float *mat1, float *mat2, float *result, 
+            int rows1, int cols1, int rows2, int cols2);
+
+/*! \brief Dot product of two vectors.
+    \param vec1 Pointer to the first vector.
+    \param vec2 Pointer to the second vector.
+    \param result Pointer to the result vector.
+    \param len Length of the vectors.
+*/
+void dot_product(float *vec1, float *vec2, float *result, int len);
+
+/*! \brief Trace of a matrix (sum of the diagonal).
+    \param matrix Pointer to the matrix. Row-major order.
+    \param result Pointer to the result.
+    \param len Length of the matrix.
+*/
+void trace(float *matrix, float *result, int len);
+
+/*! \brief Transpose a matrix.
+    \param matrix Pointer to the matrix. Row-major order.
+    \param result Pointer to the result matrix. Row-major order.
+    \param rows Number of rows in the matrix.
+    \param cols Number of columns in the matrix.
+*/
+void transpose(float *matrix, float *result, int rows, int cols);
+
+#endif // LINALG_H_
