4a8b7708773eb59d81982564194dc32540f4835d
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-20 22:25:29 +0100

    visualize the original and downsampled point clouds hovered

--- tools/viz.py
+++ tools/viz.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append(".")
 import argparse
 import open3d as o3d
+import time
 from ndtnetpp.preprocessing.ndt import ndt_downsample
 
 if __name__ == "__main__":
@@ -17,13 +18,33 @@ if __name__ == "__main__":
 
     # read input file using open3d
     pcd = o3d.io.read_point_cloud(args.input)
+    pcd.paint_uniform_color([1, 0, 0])
+
+    print("Point cloud has", len(pcd.points), "points.")
+
+    # create the visualizer
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
 
     # visualize point cloud
-    o3d.visualization.draw_geometries([pcd])
+    vis.add_geometry(pcd)
 
     # downsample the point cloud
+    # measure starting time
+    start = time.time()
     downsampled_pcd = ndt_downsample(pcd, 1000)
+    # measure ending time
+    end = time.time()
+    duration = end - start
+    hz = 1 / duration
+    print(f"Downsampling took {duration} seconds - {hz} Hz")
+    downsampled_pcd.paint_uniform_color([0, 1, 0])
 
     # visualize downsampled point cloud
-    o3d.visualization.draw_geometries([downsampled_pcd])    
-    
+    vis.add_geometry(downsampled_pcd)
+
+    # run the visualizer
+    vis.run()
+
+    # destroy the visualizer
+    vis.destroy_window()
