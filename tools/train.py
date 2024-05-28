4cc51f9e4337c4ca1fd2a05145a37ed541cff76d
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-28 22:54:19 +0100

    make a training script using dummy tensors for debugging

--- tools/train.py
+++ tools/train.py
@@ -1,2 +1,25 @@
+import torch
+import torch.nn as nn
+import sys
+sys.path.append(".")
 
-# NDT-Net++ Training Script
+from ndtnetpp.models.pointnet import PointNetClassification
+
+if __name__ == '__main__':
+    # get the device 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # create a tensor
+    x = torch.randn(2, 100000, 3).uniform_(-50.0, 50.0)
+    print(x)
+    x = x.to(device)
+
+    # create the model
+    model = PointNetClassification(3, 4096, 512)
+    model = model.to(device)
+    
+    # forward pass
+    out = model(x)
+    
+    # print the output
+    # print(out.shape)
