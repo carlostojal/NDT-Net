58e88d62739ffffbeb21583b8ed6ed30f52bbe45
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-31 17:00:14 +0100

    reimplement neighbor index computation in python

--- core1/voxel.py
+++ core1/voxel.py
@@ -1,7 +1,6 @@
 import numpy as np
-from math import ceil, floor
-from numba import njit, jit, float64, uint32, int32
-from typing import Tuple, List
+from typing import Tuple
+from enum import Enum
 
 """
  MIT License
@@ -27,6 +26,19 @@ from typing import Tuple, List
  SOFTWARE.
 
 """
+
+
+class Direction(Enum):
+    """
+    Enumeration of the possible directions.
+    """
+    X_POS=0,
+    X_NEG=1,
+    Y_POS=2,
+    Y_NEG=3,
+    Z_POS=4,
+    Z_NEG=5,
+    DIRECTION_LEN=6
 
 
 def estimate_voxel_size(num_desired_voxels: int,
@@ -79,6 +91,7 @@ def estimate_voxel_grid(max_limits: np.ndarray,
 
     return lens
 
+
 def metric_to_voxel_space(point: np.ndarray, voxel_size: float,
                           lens: np.ndarray,
                           min_limits: np.ndarray) -> np.ndarray:
@@ -103,6 +116,7 @@ def metric_to_voxel_space(point: np.ndarray, voxel_size: float,
 
     return indexes
 
+
 def voxel_to_metric_space(voxel: np.ndarray,
                           lens: np.ndarray,
                           min_limits: np.ndarray,
@@ -123,3 +137,99 @@ def voxel_to_metric_space(voxel: np.ndarray,
     point: np.ndarray = (voxel * voxel_size) + (voxel_size / 2) + min_limits
 
     return point
+
+
+def voxel_pos_to_index(voxel_pos: np.ndarray, lens: np.ndarray) -> int:
+    """
+    Convert a voxel position to an index.
+
+    Args:
+        voxel_pos (ndarray): Voxel position.
+        lens (ndarray): Voxel grid dimensions.
+
+    Returns:
+        int: Voxel index.
+    """
+
+    # check bounds
+    if np.any(voxel_pos < 0) or np.any(voxel_pos >= lens):
+        raise ValueError("Invalid position.")
+
+    # calculate the index
+    index: int = int(voxel_pos[2] * lens[0] * lens[1] + voxel_pos[1] * lens[0] + voxel_pos[0])
+
+    return index
+
+
+def index_to_voxel_pos(index: int, lens: np.ndarray) -> np.ndarray:
+    """
+    Convert a voxel index to a position.
+
+    Args:
+        index (int): Voxel index.
+        lens (ndarray): Voxel grid dimensions.
+
+    Returns:
+        ndarray: Voxel position.
+    """
+
+    # check bounds
+    if index < 0 or index >= np.prod(lens):
+        raise ValueError("Invalid index.")
+
+    # calculate the position
+    pos: np.ndarray = np.zeros(3, dtype=int)
+    pos[0] = index % lens[0]
+    pos[1] = (index % (lens[0] * lens[1])) // lens[0]
+    pos[2] = index // (lens[0] * lens[1])
+
+    return pos
+
+
+def get_neighbor_index(index: int,
+                       lens: np.ndarray,
+                       direction: Direction) -> int:
+    """
+    Get the index of the neighbor voxel in a given direction.
+
+    Args:
+        index (int): Index of the current voxel.
+        lens (ndarray): Voxel grid dimensions.
+        direction (Direction): Direction of the neighbor voxel.
+
+    Returns:
+        int: Index of the neighbor voxel.
+    """
+
+    # verify the index bounds
+    if index < 0 or index >= np.prod(lens):
+        raise ValueError("Invalid index.")
+
+    # get the direction
+    direction_vec: np.ndarray = np.zeros(3, dtype=int)
+    match direction:
+        case Direction.X_POS:
+            direction_vec[0] = 1
+        case Direction.X_NEG:
+            direction_vec[0] = -1
+        case Direction.Y_POS:
+            direction_vec[1] = 1
+        case Direction.Y_NEG:
+            direction_vec[1] = -1
+        case Direction.Z_POS:
+            direction_vec[2] = 1
+        case Direction.Z_NEG:
+            direction_vec[2] = -1
+
+    # update the index
+    cur_pos: np.ndarray = index_to_voxel_pos(index, lens)
+    cur_pos += direction_vec
+
+    # check bounds
+    if np.any(cur_pos < 0) or np.any(cur_pos >= lens):
+        raise IndexError("Invalid neighbor.")
+
+    # calculate the new index
+    new_index: int = voxel_pos_to_index(cur_pos, lens)
+
+    return new_index
