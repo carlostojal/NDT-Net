6cc8f4b8e1ee9df928b9356a29030664214742b2
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-31 19:05:03 +0100

    refactor ndt downsampling in python

--- core1/ndt.py
+++ core1/ndt.py
@@ -1,7 +1,8 @@
 import numpy as np
 from typing import List, Tuple
-from core1.normal_distributions import NormalDistribution
+from core1.normal_distributions import NormalDistribution, estimate_ndt
 from core1.kullback_leibler import KullbackLeiblerDivergence, calculate_kl_divergences
+from core1.voxel import estimate_voxel_grid
 from core1.voxel import voxel_to_metric_space
 
 """
@@ -28,6 +29,11 @@ from core1.voxel import voxel_to_metric_space
  SOFTWARE.
 
 """
+
+DOWNSAMPLE_UPPER_THRESHOLD = 0.2
+MIN_VOXEL_SIZE = 0.01
+MAX_VOXEL_SIZE = 10.0
+MAX_GUESS_ITERATIONS = 15
 
 def prune_nds(nds: np.ndarray[NormalDistribution],
               num_valid_nds: int,
@@ -116,3 +122,69 @@ def to_point_cloud(nds: np.ndarray[NormalDistribution],
                 classes.append(cls1)
 
     return np.array(points, dtype=float), np.array(covariances, dtype=float), np.array(classes, dtype=float)
+
+
+def ndt_downsample(pointcloud: np.ndarray,
+                   num_desired_points: int,
+                   classes: np.ndarray = None,
+                   num_classes: int = -1) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Downsample a point cloud using the Normal Distribution Transform (NDT).
+
+    Args:
+        pointcloud: The point cloud.
+        num_desired_points: The number of desired points.
+        classes: The classes of the points.
+        num_classes: The total number of classes.
+
+    Returns:
+        Tuple[ndarray, ndarray, ndarray]: The downsampled point cloud, the covariances and the classes.
+    """
+
+    # get the limits of the point cloud
+    min_limits = np.min(pointcloud, axis=0)
+    max_limits = np.max(pointcloud, axis=0)
+
+    # guess the voxel size
+    guess: float = (MAX_VOXEL_SIZE - MIN_VOXEL_SIZE) / 2.0
+    min_guess = MIN_VOXEL_SIZE
+    max_guess = MAX_VOXEL_SIZE
+
+    nds = None
+    lens = None
+
+    it: int = 0
+    while it <= MAX_GUESS_ITERATIONS:
+
+        # estimate the voxel grid size
+        lens = estimate_voxel_grid(max_limits, min_limits, guess)
+
+        # estimate the normal distribution transform
+        nds, num_valid_nds = estimate_ndt(pointcloud, guess, lens, min_limits, classes, num_classes)
+
+        # adjust the voxel size using binary search, until the desired number of points is reached
+        if num_valid_nds > num_desired_points * (1 + DOWNSAMPLE_UPPER_THRESHOLD):
+            min_guess = guess
+        elif num_valid_nds < num_desired_points:
+            max_guess = guess
+        else:
+            break
+        # update the guess
+        guess = (min_guess + max_guess) / 2.0
+
+        it += 1
+
+    # reached the maximum number of iterations
+    if it == MAX_GUESS_ITERATIONS:
+        raise ValueError("The maximum number of iterations was reached!")
+
+    # calculate the Kullback-Leibler divergences
+    kl_divergences, _ = calculate_kl_divergences(nds, lens)
+
+    # prune the normal distributions
+    nds = prune_nds(nds, num_valid_nds, num_desired_points, kl_divergences)
+
+    # convert the normal distributions to a point cloud
+    points, covariances, classes = to_point_cloud(nds, lens, min_limits, guess)
+
+    return points, covariances, classes
