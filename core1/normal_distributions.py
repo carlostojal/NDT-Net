776b4d7d9f4f4338c565a05d61945f79f2f99f7f
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-31 17:13:04 +0100

    implement normal distribution update in python

--- core1/normal_distributions.py
+++ core1/normal_distributions.py
@@ -27,12 +27,35 @@ import numpy as np
 
 
 class NormalDistribution:
-    def __init__(self, index: np.ndarray):
+    def __init__(self, index: np.ndarray, num_classes: int = -1):
         self.index = index
         self.mean = np.zeros(3, dtype=np.float64)
+        self.old_mean = np.zeros(3, dtype=np.float64)
         self.covariance = np.zeros((3, 3), dtype=np.float64)
         self.m2 = np.zeros(3, dtype=np.float64)  # sum of squared differences
         self.num_samples: int = 0
+        self.num_classes = num_classes
         self.class_tag: int = -1
-        self.class_samples = None  # array of samples per class
+        self.class_samples = None
+        if num_classes != -1:
+            self.class_samples = np.zeros(num_classes, dtype=np.int32)  # number of samples per class
 
+    def update(self, sample: np.ndarray, class_tag: int = -1):
+        # increment the sample count
+        self.num_samples += 1
+
+        # copy the old mean
+        self.old_mean = self.mean.copy()
+
+        # update the mean and covariance
+        self.mean += (sample - self.mean) / self.num_samples
+        self.m2 += (sample - self.mean) * (sample - self.old_mean)
+        self.covariance = self.m2 / self.num_samples
+
+        # update the class tag
+        if class_tag != -1:
+            # update the class samples
+            self.class_samples[class_tag] += 1
+
+            # update the class tag
+            self.class_tag = np.argmax(self.class_samples)
