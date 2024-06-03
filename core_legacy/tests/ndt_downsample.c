9e2b8ac8ef6f463a2a61ddb7ce55ffe5b4b8b505
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-06-03 21:29:45 +0100

    i was tracing the memory leak, i was desperate...

--- core_legacy/tests/ndt_downsample.c
+++ core_legacy/tests/ndt_downsample.c
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 #include <ndtnetpp_core/ndt.h>
 
-#define NUM_POINTS 1000
+#define NUM_POINTS 90000
 #define POINT_DIM 3
 
 #define NUM_DOWNSAMPLED_POINTS 24
 
-#define NUM_CLASSES 10
+#define NUM_CLASSES 28
 
-#define NUM_LOOPS 50
+#define NUM_LOOPS 10
 
 int main(int argc, char *argv[]) {
 
     (void)argc;
     (void)argv;
 
+    srand(0);
+
     for(unsigned int i = 0; i < NUM_LOOPS; i++) {
 
         // allocate a point cloud
         double pointcloud[NUM_POINTS * POINT_DIM] = {0};
         // fill with random values
-        srand(0);
         for(unsigned long i = 0; i < NUM_POINTS * POINT_DIM; i++) {
             pointcloud[i] = (double)rand() / RAND_MAX;
         }
@@ -59,7 +60,7 @@ int main(int argc, char *argv[]) {
         }
 
         // free the normal distributions
-        free_nds(nd_array, num_valid_nds);
+        free_nds(nd_array, len_x * len_y * len_z);
         free_kl_divergences(kl_divergences);
     }
 
